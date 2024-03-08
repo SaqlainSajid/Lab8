b43117d612eb25150fd3f97c30fe366016f206ef
ssajid <ssajid@ualberta.ca>
2024-03-08 13:33:19 -0700

    Added method getCount()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -30,10 +30,9 @@ public class CustomListTest {
     void testGetCount(){
         list = MockCityList();
         int listSize=list.getCount();
-        assertEquals(0, listSize);
         City city = new City("Regina", "Saskatchewan");
         list.add(city);
-        assertEquals(listSize+1, list.getCount());
+        assertEquals(list.getCount(),listSize+1);
     }
 
 
