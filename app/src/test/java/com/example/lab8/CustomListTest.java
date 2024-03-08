b9fa70cba7f153fe2512522fde8649e87de2e912
ssajid <ssajid@ualberta.ca>
2024-03-08 13:05:14 -0700

    Created Method addCity()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -30,6 +31,14 @@ public class CustomListTest {
         int listSize=list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize+1);
+    }
+
+    @Test
+    void testHasCity(){
+        list = MockCityList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        list.add(city);
+        assertTrue(list.hasCity(city));
     }
 
 }
